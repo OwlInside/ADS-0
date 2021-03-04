49535ea49aff4db2a204771f1c77c8cf897cb2ee
OwlInside <78905764+OwlInside@users.noreply.github.com>
2021-03-04 14:21:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    if (b==0){
-        return a;
-        return gcd(b, a % b);
-    }
-    int main() {
-        int a, b;
-        cout<<"Enter two numbers: " << endl;
-        cin>> a >> b;
-        cout<<"Gcd of your numbers is: "<< gcd( a, b ) << endl;
-        return 0;
+        while ((a != 0) && (b !=0)) {
+                if(a > b) {
+                        a = a - b;
+                }
+                else {
+                        b = b - a;
+                }
+            return (a+b);
 }
