0efb19051d8f1ae73cc1036f8e4492b48e487545
OwlInside <78905764+OwlInside@users.noreply.github.com>
2021-03-04 14:44:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,10 +2,9 @@
 
 int gcd(int a, int b) {
         while ((a != 0) && (b !=0)) {
-                if(a > b) {
+                if (a > b) {
                         a = a - b;
-                }
-                else {
+                } else {
                         b = b - a;
                 }
             return (a+b);
